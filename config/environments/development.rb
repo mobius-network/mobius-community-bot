7aed7f95c33777719252f25cbf48e18fee9c3e44
Sergey Nebolsin <sergey@nebols.in>
2018-05-24 18:20:42 +0300

    Don't read encrypted secrets in dev/test environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.read_encrypted_secrets = true
+  config.read_encrypted_secrets = false
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?

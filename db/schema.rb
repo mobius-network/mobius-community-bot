c42171fc239d060627cea37f1981fbeb5abfe562
Timur Ramazanov <charlie.vmk@gmail.com>
2018-05-31 17:58:24 +0600

    Store users last and first names in DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180522101657) do
+ActiveRecord::Schema.define(version: 20180531115524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20180522101657) do
     t.boolean "is_resident", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "first_name"
     t.index ["telegram_id"], name: "index_users_on_telegram_id", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end

e1acc0a2eb6ed60fd88142030b842da48b90adc4
Sergey Nebolsin <nebolsin@gmail.com>
2022-06-09 00:41:59 +0300

    fix: since Bot API 5.5 user ids no longer fit in 32 bits (#5)

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ Bundler.require(*Rails.groups)
 
 module MobiusBot
   class Application < Rails::Application
-    config.load_defaults 5.1
+    config.load_defaults 5.2
     config.api_only = true
     config.active_job.queue_adapter = :sucker_punch
     config.redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379')
